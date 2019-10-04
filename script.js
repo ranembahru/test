f5129db3f1ce7a4c27539a2bd635eff165985795
ranembahru <54946484+ranembahru@users.noreply.github.com>
2019-10-04 11:53:35 -0400

    trd5t

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function greet(){
 	var storach=prompt("What is your naem?");
 	alert("Hi "+storach);
 }
+function odd_or_even(){
+	var usernum=prompt("entr a number");
+	if (usernum %2==0){
+		alert("even");
+	}
+	else if (usernum %2==1) {
+		alert("oudd");
+	}
+	else{
+		alert("you did not entr a number");
+	}
+}
