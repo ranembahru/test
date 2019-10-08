0a97f0ac4ae6d529105b2985670aed3b45eedc91
ranembahru <54946484+ranembahru@users.noreply.github.com>
2019-10-08 09:13:10 -0400

    ht

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ function odd_or_even(){
 		alert("oudd");
 	}
 	else{
-		alert("you did not entr a number");                                                                                                                                                                                                                                                                                           
+		alert("you did not entr a number");
 	}
 	function the_missing_angle(){
 	var usernum=prompt("the two angle are 60% and 60%. What is the missing angle ");
-	if (usernum %60==0
+	if (usernum %60==0,
 		%60==0){
 		alert("Correct");	
 	}
