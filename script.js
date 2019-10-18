4e0970f3e66774007ddf0bf51b7872ff6555af5e
ranembahru <54946484+ranembahru@users.noreply.github.com>
2019-10-18 08:30:40 -0400

    18 oct
    8;30

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-function greet(){
-	var storach=prompt("What is your naem?");
-	alert("Hi "+storach);
-}
-function odd_or_even(){
-	var usernum=prompt("entr a number");
-	if (usernum %4==2){
-		alert("even");
+// function greet(){
+// 	var storach=prompt("What is your naem?");
+// 	alert("Hi "+storach);
+// }
+
+function o_and_v(){
+	var usernum=prompt("entr a number that is amalibal of 13");
+	for(let i=0;i<4;i++){
+	if (usernum %13==0){
+		alert("good");
 	}
-	else if (usernum %10==0) {
-		alert("oudd");
+	
+	else {
+		usernum= prompt("wromg tray agam");
 	}
-	else{
-		alert("you did not entr a number");
 	}
 }	
 
-function the_missing_angle(){
-  var usernum=prompt("the two angle are 100% and 30%. What is the missing angle ");
-  while(usernum != 50){
-      usernum=prompt("Try Again");
-  }
-	if (usernum ==50){
-	  alert("Correct");
-    }
-}
+// function the_missing_angle(){
+//   var usernum=prompt("the two angle are 100% and 30%. What is the missing angle ");
+//   while(usernum != 50){
+//       usernum=prompt("Try Again");
+//   }
+// 	if (usernum ==50){
+// 	  alert("Correct");
+//     }
+// }
 
-function test(){	
-	var sum=0;
-	var number=1;
-	while(number<=50){
-		sum+=number;		
-		number++;		
-	}
-	alert("sum = " + sum);
-}
+// function test(){	
+// 	var sum=0;
+// 	var number=1;
+// 	while(number<=50){
+// 		sum+=number;		
+// 		number++;		
+// 	}
+// 	alert("sum = " + sum);
+// }
 
-function What year?(){
-	Let year=
-	prompt+("what year?");
-    Let century=
-    alert("the ecentaey is"+ century);
+function What_year(){
+	let year= parseInt(prompt ("what year?"));
+    let century= parseInt((year) / 100) + 1;
+    alert("the ecentaey is "+ century);
 }
