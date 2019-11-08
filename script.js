d86e2b751d2713c353a173e6c479df448c4d567d
ranembahru <54946484+ranembahru@users.noreply.github.com>
2019-11-08 11:45:38 -0500

    nov 8 2019
    11:45

--- script.js
+++ script.js
@@ -1,66 +1,68 @@
-function greet(){
-	var storach=prompt("What is your naem?");
-	alert("Hi "+storach);
-}
+// function greet(){
+// 	var storach=prompt("What is your naem?");
+// 	alert("Hi "+storach);
+// }
 
-function Odd_and_Even(){
-	var usernum=prompt("entr a number that is amalibal of 13");
-	for(let i=0;i<4;i++){
-	if (usernum %13==0){
-		alert("good");
-	}
+// function Odd_and_Even(){
+// 	var usernum=prompt("entr a number that is amalibal of 13");
+// 	for(let i=0;i<4;i++){
+// 	if (usernum %13==0){
+// 		alert("good");
+// 	}
 	
-	else {
-		usernum= prompt("wromg tray agam");
-	}
-	}
-}	
+// 	else {
+// 		usernum= prompt("wromg tray agam");
+// 	}
+// 	}
+// }	
 
-function the_missing_angle(){
-  var usernum=prompt("the two angle are 100% and 30%. What is the missing angle ");
-  while(usernum != 50){
-      usernum=prompt("Try Again");
-  }
-	if (usernum ==50){
-	  alert("Correct");
-    }
+// function the_missing_angle(){
+//   var usernum=prompt("the two angle are 100% and 30%. What is the missing angle ");
+//   while(usernum != 50){
+//       usernum=prompt("Try Again");
+//   }
+// 	if (usernum ==50){
+// 	  alert("Correct");
+//     }
+// }
+
+// function test(){	
+// 	var sum=0;
+// 	var number=1;
+// 	while(number<=50){
+// 		sum+=number;		
+// 		number++;		
+// 	}
+// 	alert("sum = " + sum);
+// }
+
+// function What_year(){
+// 	let year= parseInt(prompt ("what year?"));
+//     let century= parseInt((year) / 100) + 1;
+//     alert("the ecentaey is "+ century);
+// }
+
+// function password_check(){
+//   let userName= prompt("usarnaem");
+//   let userPassword= prompt("paswrd");
+//   userLower = userName.toLowerCase();
+//   if(userLower == "ranem" && userPassword == "123"){
+//     alert("welcome");
+//   }else{
+//     alert("Hack someone else");
+//   }
+// }
+
+function up_low(){
+  let msg=prompt("ente a masge")
+   let msg2=msg.toLowerCase();
+	for(let i=0;i<msg.length;i++){
+		if (i%2==0){
+			msg2+=msg[i].toUpperCase();
+  		}
+  		else {
+  			msg2+=msg[i]
+  		}
+  	}
+  alert(msg)
 }
-
-function test(){	
-	var sum=0;
-	var number=1;
-	while(number<=50){
-		sum+=number;		
-		number++;		
-	}
-	alert("sum = " + sum);
-}
-
-function What_year(){
-	let year= parseInt(prompt ("what year?"));
-    let century= parseInt((year) / 100) + 1;
-    alert("the ecentaey is "+ century);
-}
-
-function password_check(userName, userPassword){
-  userLower = userName.toLowerCase();
-  if(userLower == "ranem" && userPassword == "123"){
-    console.log("welcome");
-  }else{
-    console.log("Hack someone else");
-  }
-}
-
-console.log(password_check("RANem", "123"))
-
-function up_low(msg){
-  let log.toLowerupCase();
-  if(msg == "soreen"){
-    console.log("welcome");
-  }else{
-    console.log("Hack someone else");
-     for(let i=0;i<4;i++);
-  }
-}
-
-console.log(msg("soreen"))
